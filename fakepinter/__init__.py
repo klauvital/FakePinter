dd05580c96419aff2ffe75fef3ad4f577c08c98d
Klauhernandez <klauvital@gmail.com>
2024-06-25 00:39:47 -0300

    Criando e atualizando arquivo requirements

--- fakepinter/__init__.py
+++ fakepinter/__init__.py
@@ -2,10 +2,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 app = Flask(__name__)
+if os.getenv("DEBUG") == 0:
+    link_banco = os.getenv("DATABASE_URL")
+else:
+    link_banco =  "sqlite:///pictures.db"
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///pictures.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = link_banco
 app.config["SECRET_KEY"] = "17d3ec830ce32f7aef7e423e0ebb9a94"
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
