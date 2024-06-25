86efb19bdac75991df9417b9e209dba0ca23815c
Klauhernandez <klauvital@gmail.com>
2024-06-25 03:19:36 -0300

    Atualizando arquivo routes com decode e encode

--- fakepinter/__init__.py
+++ fakepinter/__init__.py
@@ -5,12 +5,11 @@ from flask_bcrypt import Bcrypt
 import os
 
 app = Flask(__name__)
-if os.getenv("DEBUG") == 0:
-    link_banco = os.getenv("DATABASE_URL")
-else:
-    link_banco =  "sqlite:///pictures.db"
-
-app.config["SQLALCHEMY_DATABASE_URI"] = link_banco
+#if os.getenv("DEBUG") == 0:
+#    link_banco = os.getenv("DATABASE_URL")
+#else:
+#    link_banco =  "sqlite:///pictures.db"
+app.config["SQLALCHEMY_DATABASE_URI"] ="postgresql://bd_fakepinter_user:t7MSdPj3I8kHjypFzfWVcC7Kn4auXrnU@dpg-cpt4es6ehbks73etvup0-a.oregon-postgres.render.com/bd_fakepinter"
 app.config["SECRET_KEY"] = "17d3ec830ce32f7aef7e423e0ebb9a94"
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
